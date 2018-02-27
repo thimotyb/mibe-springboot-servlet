8bb818a5b56f04c931ba5f2fd681b990053b66ce
Thimoty Barbieri <thimoty@thimoty.it>
2018-02-28 00:02:58 +0100

    added autowired example

---
+++ src/main/java/it/unipv/mibe/course/MyService.java
@@ -0,0 +1,12 @@
+package it.unipv.mibe.course;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+
+	public String getWord() {
+		return "A serviced word.";
+	}
+	
+}
--- src/main/java/it/unipv/mibe/course/MyServlet.java
+++ src/main/java/it/unipv/mibe/course/MyServlet.java
@@ -7,16 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 @WebServlet("/myservlet")
 public class MyServlet extends HttpServlet {
 
+	@Autowired
+	MyService wordService;
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 	
 		System.out.println("Hello the servlet was called with SpringBoot.");
 		String name = request.getParameter("name");
 	    response.setContentType("text/plain");
-	    response.getWriter().write("Hello " + name + "!");
+	    response.getWriter().write("Hello " + name + "! "+wordService.getWord());
 		
 	}	
 }
