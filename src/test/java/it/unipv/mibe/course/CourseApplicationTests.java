2bba912dd57b93fb5a5505f52944ed25a5c1a193
Thimoty Barbieri <thimoty@thimoty.it>
2018-02-28 00:25:45 +0100

    Added test example

--- src/test/java/it/unipv/mibe/course/CourseApplicationTests.java
+++ src/test/java/it/unipv/mibe/course/CourseApplicationTests.java
@@ -1,7 +1,9 @@
 package it.unipv.mibe.course;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CourseApplicationTests {
 
+	@Autowired
+	MyService wordService;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void checkWordService() {
+		String answer = wordService.getWord();
+		Assert.assertTrue("I have an answer", answer.length()>0);
 	}
 
 }
