3ff185e1bbeb8c7dbda36a85657732c6608da25d
Thimoty Barbieri <thimoty@thimoty.it>
2018-03-12 10:18:09 +0100

    Added jsp support

--- src/main/java/it/unipv/mibe/course/CourseApplication.java
+++ src/main/java/it/unipv/mibe/course/CourseApplication.java
@@ -2,15 +2,23 @@ package it.unipv.mibe.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan(basePackages = { 
 	    "it.unipv.mibe.course"
 	   })
-public class CourseApplication {
+public class CourseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CourseApplication.class);
+    }
+	
 }
---
+++ src/main/java/it/unipv/mibe/course/HelloController.java
@@ -0,0 +1,15 @@
+package it.unipv.mibe.course;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/gethello")
+    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
