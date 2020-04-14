7f537f3d9ff5b8b87cc76f73fdebfceb389b083f
Thimoty Barbieri <thimoty@thimoty.it>
2020-04-14 15:08:47 +0200

    Aggiunta post

--- src/main/java/it/unipv/mibe/course/HelloController.java
+++ src/main/java/it/unipv/mibe/course/HelloController.java
@@ -1,16 +1,21 @@
 package it.unipv.mibe.course;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import it.unipv.mibe.dao.PersonDAO;
 import it.unipv.mibe.entity.Person;
 
-@Controller
+@RestController
 public class HelloController {
 	
 	@Autowired
@@ -33,6 +38,15 @@ public class HelloController {
         all.forEach(p -> sb.append(p.getFullName() + "<br>"));
  
         return sb.toString();
+    }
+    
+    @PostMapping("/addperson")
+    public String addPerson(@RequestBody Person p) {
+    	
+    	System.out.println("Posted person: "+p.getFullName());
+    	p = personDAO.save(p);
+    	
+    	return "{ \"message\": \"Created person "+p.getId()+".\"}";
     }
     
 }
